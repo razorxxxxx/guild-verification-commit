3799423466f7cd5228ae3c423b18cefe15c8abfb
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:12:39 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:12:34.106Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-26T22:12:39.105Z
+Commit: Fix memory leak in event handlers
