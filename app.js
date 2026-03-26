5b0697890eb766f89f7c16361f3af57b4079284b
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:24:28 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:19:10.840Z
-Commit: Add multi-language support
+Timestamp: 2026-03-26T22:24:27.968Z
+Commit: Resolve timezone issues in date handling
