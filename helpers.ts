9fbb61fbe32794b23a8bb23b5fb180e2f7545d47
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:13:15 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:13:13.247Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-26T22:13:15.311Z
+Commit: Fix CSS styling bug on mobile devices
