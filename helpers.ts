6cc891b850229648e521c7ca3eaf7a8324fb2a62
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:15:13 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:13:39.598Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-26T22:15:13.196Z
+Commit: Resolve timezone issues in date handling
