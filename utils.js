74a60b72513989ce626e5176ca12b120a4e6c520
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:12:31 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:12:28.043Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-26T22:12:30.980Z
+Commit: Fix CSS styling bug on mobile devices
