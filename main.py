10fd8a47c26bd5f1e8e7e0ef0f25b5efea6fd2e9
razorxxxxx <163322136+razorxxxxx@users.noreply.github.com>
2026-03-27 01:13:51 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-26T22:13:48.895Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-26T22:13:50.941Z
+Commit: Resolve null pointer exception in user service
